bdef2eff75e970dff55f70239aa5a60a1a5cc0a4
alielrayes <engali7assan@yahoo.com>
2022-12-01 20:22:25 +0200

    first commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,52 @@
+import Header from "../comp/header";
+import Footer from "../comp/Footer";
+import Loading from "../comp/Loading";
+
+import { Helmet } from "react-helmet-async";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase/config";
+import Erroe404 from "../pages/erroe404";
+
+const About = () => {
+  const [user, loading, error] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user && !loading) {
+      navigate("/");
+    }
+
+    if (user) {
+      if (!user.emailVerified) {
+        navigate("/");
+      }
+    }
+  });
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <Erroe404 />;
+  }
+
+  if (user) {
+    if (user.emailVerified) {
+      return (
+        <>
+          <Helmet>
+            <title>About Page</title>
+          </Helmet>
+          <Header />
+          <main>About page</main>
+          <Footer />
+        </>
+      );
+    }
+  }
+};
+
+export default About;
