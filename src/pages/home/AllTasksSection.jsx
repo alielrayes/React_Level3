8577b6671c53b4a2a066a3d3c73e5fad13dae220
alielrayes <engali7assan@yahoo.com>
2022-12-14 03:16:11 +0200

    lesson18.1

--- src/pages/home/AllTasksSection.jsx
+++ src/pages/home/AllTasksSection.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { collection, limit, orderBy, query } from "firebase/firestore";
@@ -7,11 +7,10 @@ import ReactLoading from "react-loading";
 import Moment from "react-moment";
 
 const AllTasksSection = ({ user }) => {
-  const [value, loading, error] = useCollection(
-
-    query(    collection(db, user.uid), orderBy("id", "asc"),      )
-    
-    );
+  const [initialData, setinitialData] = useState(
+    query(collection(db, user.uid), orderBy("id", "asc"))
+  );
+  const [value, loading, error] = useCollection(initialData);
 
   if (error) {
     return <h1>ERROR</h1>;
@@ -29,34 +28,64 @@ const AllTasksSection = ({ user }) => {
     console.log(value.docs);
 
     return (
-      <section className="flex all-tasks mt">
-        {value.docs.length === 0 && (
-          <h1>Congratulations! You have completed your tasks ♥</h1>
-        )}
+      <div>
+        {/* OPIONS (filtered data) */}
+        <section style={{justifyContent: "center"}} className="parent-of-btns flex mtt">
+          <button
+            onClick={(params) => {
+              setinitialData(
+                query(collection(db, user.uid), orderBy("id", "desc"))
+              );
+            }}
+          >
+            Newest first
+          </button>
 
-        {value.docs.map((item) => {
-          return (
-            <article key={item.data().id} dir="auto" className="one-task">
-              <Link className="task-link" to={`/edit-task/${item.data().id}`}>
-                <h2> {item.data().title} </h2>
-                <ul>
-                  {item.data().details.map((item, index) => {
-                    if (index < 2) {
-                      return <li key={item}> {item} </li>;
-                    } else {
-                      return false;
-                    }
-                  })}
-                </ul>
+          <button
+            onClick={(params) => {
+              setinitialData(
+                query(collection(db, user.uid), orderBy("id", "asc"))
+              );
+            }}
+          >
+            Oldest first
+          </button>
+          <select id="browsers">
+            <option value="ddddd"> All Tasks </option>
+            <option value="dddddd"> Completed </option>
+            <option value="dddddd"> Not Completed </option>
+          </select>
+        </section>
 
-                <p className="time">
-                  <Moment fromNow date={item.data().id} />
-                </p>
-              </Link>
-            </article>
-          );
-        })}
-      </section>
+        <section className="flex all-tasks mt">
+          {value.docs.length === 0 && (
+            <h1>Congratulations! You have completed your tasks ♥</h1>
+          )}
+
+          {value.docs.map((item) => {
+            return (
+              <article key={item.data().id} dir="auto" className="one-task">
+                <Link className="task-link" to={`/edit-task/${item.data().id}`}>
+                  <h2> {item.data().title} </h2>
+                  <ul>
+                    {item.data().details.map((item, index) => {
+                      if (index < 2) {
+                        return <li key={item}> {item} </li>;
+                      } else {
+                        return false;
+                      }
+                    })}
+                  </ul>
+
+                  <p className="time">
+                    <Moment fromNow date={item.data().id} />
+                  </p>
+                </Link>
+              </article>
+            );
+          })}
+        </section>
+      </div>
     );
   }
 };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -167,17 +167,7 @@ const Home = () => {
           <Header />
 
           <main className="home">
-            {/* OPIONS (filtered data) */}
-            <section className="parent-of-btns flex mtt">
-              <button>Newest first</button>
 
-              <button>Oldest first</button>
-              <select id="browsers">
-                <option value="ddddd"> All Tasks </option>
-                <option value="dddddd"> Completed </option>
-                <option value="dddddd"> Not Completed </option>
-              </select>
-            </section>
 
             {/* SHOW all tasks */}
             <AllTasksSection user={user} />
